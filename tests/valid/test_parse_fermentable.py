44fb9527d390c49fbe79fd691c53f4e27916b20f
YourOwnBeer <brew@yourownbeer.de>
2024-03-16 11:43:15 -0400

    single fermentable

--- tests/valid/test_parse_fermentable.py
+++ tests/valid/test_parse_fermentable.py
@@ -3,6 +3,12 @@ import os
 from beerxml import BeerxmlParser
 
 
+def test_parse_one_hop() -> None:
+    with open(os.path.join(os.path.dirname(__file__), "../assets/valid/fermentable/valid_fermentable_single.xml")) as file_content:
+        xml_content = file_content.read()
+    BeerxmlParser().parse(xml_content)
+
+
 def test_parse_fermentable_type_adjunct() -> None:
     with open(os.path.join(os.path.dirname(__file__), "../assets/valid/fermentable/valid_fermentable_type_adjunct.xml")) as file_content:
         xml_content = file_content.read()
