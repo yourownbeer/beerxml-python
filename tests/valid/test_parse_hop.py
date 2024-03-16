657fa70d082767bf4d06f0f2a1d3117ebec0293b
YourOwnBeer <brew@yourownbeer.de>
2024-03-16 11:28:48 -0400

    valid hops

--- tests/valid/test_parse_hop.py
+++ tests/valid/test_parse_hop.py
@@ -31,3 +31,39 @@ def test_parse_hop_use_aroma() -> None:
     with open(os.path.join(os.path.dirname(__file__), "../assets/valid/hop/valid_hop_use_aroma.xml")) as file_content:
         xml_content = file_content.read()
     BeerxmlParser().parse(xml_content)
+
+
+def test_parse_hop_form_leaf() -> None:
+    with open(os.path.join(os.path.dirname(__file__), "../assets/valid/hop/valid_hop_form_leaf.xml")) as file_content:
+        xml_content = file_content.read()
+    BeerxmlParser().parse(xml_content)
+
+
+def test_parse_hop_form_pellet() -> None:
+    with open(os.path.join(os.path.dirname(__file__), "../assets/valid/hop/valid_hop_form_pellet.xml")) as file_content:
+        xml_content = file_content.read()
+    BeerxmlParser().parse(xml_content)
+
+
+def test_parse_hop_form_plug() -> None:
+    with open(os.path.join(os.path.dirname(__file__), "../assets/valid/hop/valid_hop_form_plug.xml")) as file_content:
+        xml_content = file_content.read()
+    BeerxmlParser().parse(xml_content)
+
+
+def test_parse_hop_type_aroma() -> None:
+    with open(os.path.join(os.path.dirname(__file__), "../assets/valid/hop/valid_hop_type_aroma.xml")) as file_content:
+        xml_content = file_content.read()
+    BeerxmlParser().parse(xml_content)
+
+
+def test_parse_hop_type_bittering() -> None:
+    with open(os.path.join(os.path.dirname(__file__), "../assets/valid/hop/valid_hop_type_bittering.xml")) as file_content:
+        xml_content = file_content.read()
+    BeerxmlParser().parse(xml_content)
+
+
+def test_parse_hop_type_both() -> None:
+    with open(os.path.join(os.path.dirname(__file__), "../assets/valid/hop/valid_hop_type_both.xml")) as file_content:
+        xml_content = file_content.read()
+    BeerxmlParser().parse(xml_content)
